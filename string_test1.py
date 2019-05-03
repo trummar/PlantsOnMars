848fcd6a8d2a69a7198a7df370df0258c482586a
trummar <50231865+trummar@users.noreply.github.com>
2019-05-03 14:09:10 +0200

    Add files via upload

---
+++ string_test1.py
@@ -0,0 +1,14 @@
+fornavn = "Elvis"
+etternavn = "Presley"
+
+print(fornavn, etternavn) # hashtag er en måte å gi kommentarer i teksten
+
+alder = "90"  #en variabel kan se ut som et tall men være en streng
+
+print(fornavn, etternavn, alder)
+
+test = alder/2  # dette gir oss feil fordi her er alder en streng,
+print(test)     # og ikke et tall
+
+
+
