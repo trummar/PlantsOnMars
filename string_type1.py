482db6b2e8f5c68f0ab31cb0a13d9a84bd47b28a
trummar <50231865+trummar@users.noreply.github.com>
2019-05-03 15:21:34 +0200

    Add files via upload

---
+++ string_type1.py
@@ -0,0 +1,11 @@
+var1 = 5
+var2 = "7"
+print(type(var2)) # gir output om at var2 er en streng
+
+#Du kan gjøre om ved å bruke int()-funksjonen. Du skriver:
+
+var2 = int(var2) # Da gjør den nemlig om var2 til helttall fra streng
+print(type(var2))
+var3 = var1 * var2
+print(var3)
+print(type(var3))
