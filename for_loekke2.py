075dc96aee97344815ddc90f2b4b0c92dbac2df0
trummar <50231865+trummar@users.noreply.github.com>
2019-05-03 20:02:45 +0200

    Add files via upload

---
+++ for_loekke2.py
@@ -0,0 +1,17 @@
+for i in range(10):
+    print(i)
+
+print()             # ekstra linje for synlighetens skyld
+    
+for j in range(1,10):
+    print(j)
+
+print()             # ekstra linje for synlighetens skyld
+
+for k in range(1,20,2):
+    print(k)
+
+print()             # ekstra linje for synlighetens skyld 
+
+
+    
